a2db61da90a32ca618a9a2ff2780304f7ac98e52
Adj0803 <adj0803@gmail.com>
2022-07-21 03:25:48 -0400

    js

--- assets/css/assets/js/homepage.js
+++ assets/css/assets/js/homepage.js
@@ -1,5 +1,21 @@
-var getUsersRespos = function() {
-    fetch("function was called");
+var getUsersRespos = function(user) {
+    // format the github api url
+var apiurl = "https://api.github.com/users/" + user + "/repos";
+
+    //make a request to the url
+fetch(apiUrl).then(function(response) {    response.json().then(function(data) {
+    response.json().then(function(response) {   
+        console.log(data);
+        });
+    });
+});
+console.log("outside");
+}
+var userFormE1 = document.querySelector("#user-form");
+var nameInputE1 = document.querySelector("#user-name");
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
 };
 
-getUsersRespos();
+userFormE1.addEventListener("submit", formSubmitHandler);
